ce59f4a883d5fb214efd87f249ccb006d722fc55
susmina94 <manandharsushi@gmail.com>
2024-06-30 16:43:30 +0545

    Making button clickable

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// const regBtn = document.getElementById("btnRegistration");
+// regBtn.addEventListener("click", registration_success_msg);
+
+function registration_success_msg(){
+    alert("We are launching soon. Thank you for visiting.");
+}
