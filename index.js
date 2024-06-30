cef275e647f9787af8c07e58f2fcb7b4ed4fdbf6
susmina94 <manandharsushi@gmail.com>
2024-06-30 17:27:34 +0545

    duplicated mouse event actions on both buttons

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ regBtn.addEventListener('mouseout',function(){
     regBtn.classList.remove('btn--primary-hover');
 });
 
+const regBtn2 = document.getElementById("btnRegistration2");
+function registration_success_msg(){
+    alert("We are launching soon. Thank you for visiting.");
+}
+// clicking the register button
+regBtn2.addEventListener('click',registration_success_msg);
+
+regBtn2.addEventListener('mouseover',function(){
+    regBtn2.classList.add('btn--primary-hover');
+});
+regBtn2.addEventListener('mouseout',function(){
+    regBtn2.classList.remove('btn--primary-hover');
+});
