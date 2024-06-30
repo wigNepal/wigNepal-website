5d190ea4abf7216e6da2914f3c83449503a4bc33
susmina94 <manandharsushi@gmail.com>
2024-06-30 17:14:36 +0545

    added mouseevents on button

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
-// const regBtn = document.getElementById("btnRegistration");
-// regBtn.addEventListener("click", registration_success_msg);
-
+const regBtn = document.getElementById("btnRegistration");
 function registration_success_msg(){
     alert("We are launching soon. Thank you for visiting.");
 }
+// clicking the register button
+regBtn.addEventListener('click',registration_success_msg);
+
+regBtn.addEventListener('mouseover',function(){
+    regBtn.classList.add('btn--primary-hover');
+});
+regBtn.addEventListener('mouseout',function(){
+    regBtn.classList.remove('btn--primary-hover');
+});
+
